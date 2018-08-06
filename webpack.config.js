49bb756066750bcd381dfa65f03aa0bf7bb46670
loveKino <842914439@qq.com>
2018-08-07 04:36:44 +0800

    re-design

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,60 @@
 'use strict';
 
-module.exports = {
-    entry: {
-        app: './lib/index.js'
-    },
+const webpack = require('webpack');
 
-    output: {
-        path: __dirname + '/asset',
-        filename: '[name].js'
+module.exports = {
+  mode: process.env.DEV ? 'development' : 'production',
+  entry: {
+    app: './lib/index.js'
+  },
+
+  devtool: 'source-map',
+  output: {
+    path: __dirname + '/asset',
+    filename: '[name].js',
+    publicPath: '/assets'
+  },
+
+  module: {
+    rules: [{
+      test: /\.jsx?$/,
+      exclude: /node_modules/,
+      use: {
+        loader: 'babel-loader',
+        options: {
+          presets: ['react', 'babel-preset-env'],
+          plugins: ['wildcard']
+        }
+      }
+    }, {
+      test: /\.(woff|woff2)(\?v=\d+\.\d+\.\d+)?$/,
+      loader: 'url?limit=10000&mimetype=application/font-woff'
+    }, {
+      test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
+      loader: 'url?limit=10000&mimetype=application/octet-stream'
+    }, {
+      test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
+      loader: 'file'
+    }, {
+      test: /\.svg/,
+      loader: 'url-loader?limit=10000&mimetype=image/svg+xml'
+    }]
+  },
+
+  plugins: [
+    new webpack.HotModuleReplacementPlugin()
+  ],
+
+  devServer: {
+    contentBase: __dirname + '/dist',
+    hot: true,
+    progress: true,
+    port: 8080,
+    proxy: {},
+    watchContentBase: true,
+    watchOptions: {
+      poll: true,
+      ignored: /node_modules/
     }
+  }
 };
